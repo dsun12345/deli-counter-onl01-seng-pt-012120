fc48b2c570a92b1cbc58752b80918f80383aa748
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 14:23:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def now_serving (array)
   if array == 0 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving "
+    puts "Currently serving #{array[0]}"
   end 
 end
