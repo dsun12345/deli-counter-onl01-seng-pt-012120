5b436d24aee89a0eb0de3ae98455add7fa526248
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 14:10:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line (array)
+  if array.size == 0
+     puts "The line is currently empty."
+  else
+    string = String.new
+    arr = []
+    array.each_with_index {|name,number| arr << "#{number+1}. #{name}"}
+    string << arr.join(" ")
+    puts "The line is currently: " + string
+  end 
+end 
+
+def take_a_number(array, name)
+  if array.size == 0
+    array = array.push(name)
+    array.each_with_index do |n, i|   
+    puts  "Welcome, #{n}. You are number #{i + 1} in line."
+    end 
+  else
+   array = array.push(name)
+    array.each_with_index do |n, i|   
+    puts  "Welcome, #{n}. You are number #{i + 1} in line."
+    end 
+  end
+end
+
